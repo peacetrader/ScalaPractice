efcf7a24f6be620285baeccd6664ba34b5b0d03f
purnima.shanti <purnima.shanti@digital.hmrc.gov.uk>
2016-11-05 22:08:07 +0000

    if else test

---
+++ src/test/IfElse.scala
@@ -0,0 +1,14 @@
+package test
+
+/**
+  * Created by apple on 05/11/2016.
+  */
+class IfElse {
+  def main(args: Array[String]) {
+    var x = 10;
+
+    if( x < 20 ){
+      println("This is if statement");
+    }
+  }
+}
