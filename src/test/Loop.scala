11cd5e43c74ebf0d1b20b99cef8e1519b7590446
purnima.shanti <purnima.shanti@digital.hmrc.gov.uk>
2016-11-06 20:30:19 +0000

    adding loop test

---
+++ src/test/Loop.scala
@@ -0,0 +1,16 @@
+package test
+
+/**
+  * Created by apple on 06/11/2016.
+  */
+object Loop {
+  def main(args: Array[String]) {
+    var a = 10;
+
+    // An infinite loop.
+    while( true ){
+      println( "Value of a: " + a );
+    }
+  }
+}
+
